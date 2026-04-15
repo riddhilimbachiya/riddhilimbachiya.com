aeafc6477b752979a877cf073a0a651c1fa140d3
Riddhi <riddhiilimbachiya@gmail.com>
2026-04-15 16:44:18 +0530

    🦔 updates posthog config

--- src/components/providers/posthog-provider.tsx
+++ src/components/providers/posthog-provider.tsx
@@ -3,8 +3,12 @@
 import posthog from 'posthog-js';
 import { PostHogProvider as PHProvider } from 'posthog-js/react';
 import { useEffect } from 'react';
+import { usePathname, useSearchParams } from 'next/navigation';
+
 
 export function PostHogProvider({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
   useEffect(() => {
     if (
       typeof window !== 'undefined' &&
@@ -14,11 +18,23 @@ export function PostHogProvider({ children }: { children: React.ReactNode }) {
       posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
         api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
         person_profiles: 'always',
-        capture_pageview: true,
-        capture_pageleave: true,
+        autocapture: false,
+        capture_pageview: false,
       });
     }
   }, []);
+
+  useEffect(() => {
+    if ( typeof window !== 'undefined' &&
+      process.env.NEXT_PUBLIC_POSTHOG_KEY &&
+      process.env.NODE_ENV === 'production') {
+      posthog.capture('$pageview', {
+        $current_url: window.location.href,
+      });
+    }
+  }, [pathname, searchParams]);
+
+  
 
   return <PHProvider client={posthog}>{children}</PHProvider>;
 }
